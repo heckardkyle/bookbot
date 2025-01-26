6326ac51c89b2c15cc0c15388d0cb0f2936e4c61
heckardkyle <heckardkyle@yahoo.com>
2025-01-25 23:43:10 -0800

    Create nice report and count only alpha characters

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-        print(word_count(file_contents))
-        character_dict = char_count(file_contents)
-        for character in character_dict:
-            print(f"'{character}': {character_dict[character]}")
-
 def word_count(file_contents):
+    # return word count of file string
     word_count = file_contents.split()
     return len(word_count)
 
+def sort_on(dict):
+    return dict["num"]
+
 def char_count(file_contents):
+    # return list that counts occurence of each alpha character
     contents_lowered = file_contents.lower()
+
     character_dict = {}
     for character in contents_lowered:
-        if character not in character_dict:
+        if (character not in character_dict) and (character.isalpha()):
             character_dict[character] = 1
-        else:
+        elif character.isalpha():
             character_dict[character] += 1
-    return character_dict
 
+    character_list = []
+    for key in character_dict:
+        dict = {'char': key, 'num': character_dict[key]}
+        character_list.append(dict)
+
+    character_list.sort(key=sort_on, reverse=True)
+
+    return character_list
+
+def main():
+    # read file and print report
+    with open(file_to_open) as f:
+        file_contents = f.read()
+
+    print(f"--- Begin report of {file_to_open} ---")
+    print(f"{word_count(file_contents)} words found in the document")
+    print()
+
+    character_list = char_count(file_contents)
+    for character in character_list:
+        print(f"The '{character['char']}' character was found {character['num']} times")
+    
+    print("--- End report ---")
+
+file_to_open = "books/frankenstein.txt"
 
 main()
